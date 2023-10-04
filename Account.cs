e400adf593931e85265b6f216403490bdf40b059
Nabi <he000123@algonquinlive.com>
2023-10-04 17:16:54 -0400

    Initial commit

---
+++ Account.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class Account
+    {
+        public int AccountNumber { get; } //'get'the meaning of can't be modified externally.
+        public string OwnerName { get; set; }
+        public double Balance { get; set; }//ensures the Balance can only be modified within the Account class. 
+        public double MonthlyDepositAmount { get; set; } // only use "get" property becomes read-only
+        
+
+        public static double MonthlyFee  = 4.0;
+        public static double MonthlyInterestRate  = 0.0025;
+        public static double MinimumInitialBalance  = 1000;
+        public static double MaximumMonthDeposit  = 50;
+
+        public Account(string ownerName, double initialDeposit,  double monthlyDeposit) 
+        { 
+            this.OwnerName = ownerName;
+            this.Balance = initialDeposit;
+            this.MonthlyDepositAmount = monthlyDeposit;
+            this.AccountNumber = RandomGeneratedNumber();
+            
+        }
+
+        private static int RandomGeneratedNumber()
+        {
+            var random = new Random();
+            return random.Next(90000, 99999);
+        }
+        public double Deposit (double amount)
+        {
+            Balance += amount;
+            return Balance;
+        }
+        public double withdraw(double amount)
+        {
+            Balance -= amount;
+            return Balance;
+        }
+
+
+        public void UpdateBalance()
+        {
+            for (int i = 0; i < 8; i++) 
+            {
+                Balance -= MonthlyFee;
+                Balance += Balance * MonthlyInterestRate;
+                Balance += MonthlyDepositAmount;
+            }
+        }
+    }
+}
---
+++ Bank.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class Bank
+    {
+        static void Main(string[] args)
+        {
+            List<Account> accounts = new List<Account>();
+            Console.WriteLine("Enter the number of months to deposit: ");
+            string monthsInput = Console.ReadLine();
+            while (true) 
+            {
+
+                Console.WriteLine("Enter the Customer's name: ");
+                string name = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(name))
+                { break; }
+
+                int months = int.Parse(monthsInput);
+
+                Console.WriteLine($"Enter {name}'s initial Deposit Amount (minimum $1,000.00): ");
+                double initialDeposit = double.Parse (Console.ReadLine()); //used to convert the string input to a double data type.
+
+                Console.WriteLine($"Enter {name}'s Monthly Deposit Amount (minimum $50.00): ");
+                double monthlyDeposit = double.Parse(Console.ReadLine());
+
+                Account account = new Account(name, initialDeposit, monthlyDeposit);
+                accounts.Add(account);
+            }
+
+            foreach (Account account in accounts)
+            {
+                account.UpdateBalance();
+                Console.WriteLine($"After 8 months, {account.OwnerName}'s account #{account.AccountNumber}, has a balance of: ${account.Balance.ToString("0.00")}");//The balance is displayed with two decimal places.
+
+            }
+            Console.WriteLine("Press Enter to complete.");
+            Console.Read();
+        }
+    }
+}
